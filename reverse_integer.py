6f717f4aea095fd69d9d40bca49b3d6f4eeddffc
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2021-04-30 15:51:39 +0530

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,22 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        revnum = 0
-        if x<0:
-                return (-1)*Solution().reverse(-x)
-        else:
-            remainder = 0
-            i = 0
-                
-            while x!=0:
-                remainder = x%10
-                x=x//10
-                revnum = revnum*10 + remainder
-            
-        if -2**(31) <= revnum <= 2**(31) - 1:
-            return revnum
-            
-        else:
+        revnum = reversal(x)
+        if (-(2 ** 31) > revnum) or (revnum > (2 ** 31) - 1):
             return 0
-            
-
+        else:
+            return revnum        
         
+def reversal(x):
+    revnum = 0
+    if x<0:
+        return (-1)*Solution().reverse(-x)
+    else:
+        remainder = 0
+        i = 0  
+        while x!=0:
+            remainder = x%10
+            x=x//10
+            revnum = revnum*10 + remainder
+    return revnum
