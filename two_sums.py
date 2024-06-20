9d9604736b6d3c393dcfcc2a9689974ece67c08b
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-06-20 18:10:24 -0400

    Update two_sums.py

--- two_sums.py
+++ two_sums.py
@@ -1,7 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for j in range(0,len(nums)):
-            first = nums[j]
-            for i in range(j+1, len(nums)):
-                if target==first+nums[i]:
-                    return [j,i]
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictNums = {}
+
+        for i in range(len(nums)):
+            if nums[i] in dictNums:
+                dictNums[nums[i]].append(i)
+            else:
+                dictNums[nums[i]] = [i]
+
+        for key, item in dictNums.items():
+            complement = target - key
+            if complement in dictNums:
+                if len(dictNums[complement]) > 1:
+                    return dictNums[complement]
+                elif complement != key:
+                    l = item + dictNums[complement]
+                    return l
