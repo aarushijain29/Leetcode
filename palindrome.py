b396b9afae24c72f68b5e2036593a68c86bced82
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-06-22 15:37:00 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,28 +1,10 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        length = length_calc(x)
-        reverse_num = reverse(x, length)
-        if x<0:
-            return False
-        else:
-            if reverse_num==x:
-                return True
-            else:
-                return False
-        
-def length_calc(num):
-    length = 0
-    if num==0:
-        length+=1
-    while num>0:
-        num=num//10 
-        length+=1 
-    return length
+    def isPalindrome(self, s: str) -> bool:
+        a = ""
 
-def reverse(num, len):
-    reverse_num = 0
-    for i in range(len, 0,-1):
-        remainder = num%10
-        reverse_num+=remainder*(10**(i-1))
-        num=num//10
-    return reverse_num
+        for char in s:
+            if char.isalnum():
+                a += char
+        a = a.lower()
+        a = a.replace(" ", "")
+        return a[::-1] == a
