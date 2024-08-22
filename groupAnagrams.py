9e5ba7e92bec25b28ddc52f31e0f3219cd6f67ea
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-08-22 19:42:35 +0530

    Update groupAnagrams.py

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,4 +1,5 @@
 class Solution:
+    # tracking frequence of alphabets in each string to group anagrams i.e. words which have the same frequency of letters
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) in [0,1]:
             return [strs]
@@ -12,3 +13,13 @@ class Solution:
             res[tuple(count)].append(s)
 
         return res.values()
+    # sorting words in strs to group anagrams i.e. words which are the same after sorting
+    def groupAnagramsUsingSort(self, strs: List[str]) -> List[List[str]]:
+        
+        d = defaultdict(list)
+
+        for s in strs:
+            sorted_s = str(sorted(s))
+            d[sorted_s].append(s)
+
+        return d.values()
