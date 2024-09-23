50d6622a7ee6260d532de65ef16d70d86ca1b6dd
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-09-23 18:15:18 -0400

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,14 @@
 class Solution:
 
-    def search_help(self, nums: List[int], target: int, start: int, end: int) -> int:
-
-        if start <= end:
-            mid = (start + end)//2
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        res = -1
+        while l <= r:
+            mid = int(l + (r - l)/2)
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                return self.search_help(nums, target, 0, mid - 1)
+            elif nums[mid] < target:
+                l = mid + 1
             else:
-                return self.search_help(nums, target, mid + 1, end)
-
-        return -1
-
-    def search(self, nums: List[int], target: int) -> int:
-        return self.search_help(nums, target, 0, len(nums) - 1)
+                r = r = mid - 1
+        return res
