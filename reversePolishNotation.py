ecb5e0218e77b3d7c6a7689cbf8e8820e7a6165e
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-10-02 18:56:12 -0400

    Create reversePolishNotation.py

---
+++ reversePolishNotation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operators = ['+', '-', '*', '/']
+        for token in tokens:
+            if token in operators:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                if token == operators[0]:
+                    stack.append(op1 + op2)
+                elif token == operators[1]:
+                    stack.append(op1 - op2)
+                elif token == operators[2]:
+                    stack.append(op1 * op2)
+                else:
+                    stack.append(int(op1 / op2))
+            else:
+                stack.append(int(token))
+
+        return stack[0]
