41604ca6bbfd34037eebb717dcabde70252ddccc
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2024-10-17 20:06:52 -0400

    Update and rename invert_binary_tree.py to invertBinaryTree.py

--- invertBinaryTree.py
+++ invertBinaryTree.py
@@ -7,11 +7,7 @@
 class Solution:
 
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
         if root:
-            self.invertTree(root.right)
-            self.invertTree(root.left)
-            root.left, root.right = root.right, root.left
-
-        return root            
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         
+        return root
