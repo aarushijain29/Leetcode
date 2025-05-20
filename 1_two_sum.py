36069dfcd962afb7fc9d00531d6938e9e81037a9
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2025-05-20 19:35:20 -0400

    Update and rename twoSum.py to 1_two_sum.py

---
+++ 1_two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in nums_map:
+                return [i, nums_map[complement]]
+            nums_map[nums[i]] = i
+        
+        return []
