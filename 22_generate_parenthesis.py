ea76fb77ae42e194dbb4d6f2631e0b7b02b8dd78
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2025-05-21 23:26:38 -0400

    Update and rename generateParenthesis.py to 22_generate_parenthesis.py

---
+++ 22_generate_parenthesis.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+
+        def backtrack(open_b, close_b):
+            if open_b == close_b == n:
+                res.append(''.join(stack))
+                return
+            
+            if open_b < n:
+                stack.append('(')
+                backtrack(open_b + 1, close_b)
+                stack.pop()
+                
+            if close_b < open_b:
+                stack.append(')')
+                backtrack(open_b, close_b + 1)
+                stack.pop()
+        
+        backtrack(0, 0)
+        return res
+            
