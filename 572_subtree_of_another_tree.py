a73bf62009bb3c864a9a185a82b7131ca4ca9cc5
Aarushi Jain <62868606+aarushijain29@users.noreply.github.com>
2025-05-29 23:16:57 -0400

    Update and rename isSubtree.py to 572_subtree_of_another_tree.py

--- 572_subtree_of_another_tree.py
+++ 572_subtree_of_another_tree.py
@@ -5,18 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p, q) -> bool:
+    def isSameTree(self, p, q):
         if p and q and p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return not p and not q
 
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
+        if not root: return False
+        if not subRoot: return True
+        if self.isSameTree(root, subRoot):
             return True
-        if not root:
-            return False
-
-        if root and subRoot:
-            if root.val == subRoot.val and self.isSameTree(root, subRoot):
-                return True 
-            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
